d712586ac51f40aa92949a163504e471c3d8a246
MMohamed-0 <mohamedmo27k@gmail.com>
2025-12-18 17:32:29 +0000

    Update recon.sh

--- tools-and-scripts/bash/recon-script/recon.sh
+++ tools-and-scripts/bash/recon-script/recon.sh
@@ -1,1 +1,30 @@
+#!bin/bash
+
+ #  Simple automated reconaissnce script
+ #  Author: Mohamed Mohamed
+ #  Usage: bash recon.sh  < Target >
+
+TARGET=$1
+OUTPUT_DIR="recon-results-$TARGET"
+
+if [ -z "$TARGET" ]; then
+  echo "Usage: $0 <target>"
+  exit 1
+fi
+
+mkdir $OUTPUT_DIR
+
+echo "[+] Starting reconnaissance on $TARGET"
+
+echo "[+] Pinging target ..."
+ping -c 4 $TARGET > $OUTPUT_DIR/ping.txt
+
+echo "[+] Running nmap scan..."
+nmap -sS -Pn $TARGET > $OUTPUT_DIR/nmap.txt
+
+echo "[+] Running whois..."
+whois $TARGET > $OUTPUT_DIR/nslookup.txt
+
+echo "[+] Reconnaissance complete."
+echo "[+] Results saved in $OUTPUT_DIR"
 
